11ae739fd5502219103238013543427ab01f4701
Diego Hernandez <hcdiego2012@gmail.com>
2025-02-22 14:21:32 -0600

    Base para el proyecto terminada lista para implementar todo

--- app/src/main/java/com/example/quizzapp_project/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp_project/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.quizzapp_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnJugar: Button
+    private lateinit var btnOpciones: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnJugar = findViewById(R.id.btnPlay)
+        btnOpciones = findViewById(R.id.btnOptions)
+
+        btnJugar.setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
+        btnOpciones.setOnClickListener {
+            val intent = Intent(this, ActivitySettings::class.java)
+            startActivity(intent)
         }
     }
 }
