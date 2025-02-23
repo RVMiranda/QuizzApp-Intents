18a282e5b952fc41d95b669ed0cac85a45a86094
Diego Hernandez <hcdiego2012@gmail.com>
2025-02-22 18:25:08 -0600

    Texto de correcto o incorrecto agregado, igualmente se inicializó los botones o texto en el archivo PlayActivity.kt para empezar a implementar sus funcionalidades.

--- app/src/main/java/com/example/quizzapp_project/PlayActivity.kt
+++ app/src/main/java/com/example/quizzapp_project/PlayActivity.kt
@@ -2,6 +2,8 @@ package com.example.quizzapp_project
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,39 @@ import androidx.core.view.WindowInsetsCompat
 
 class PlayActivity : AppCompatActivity() {
     private lateinit var hintsBtn : Button
+    private lateinit var numQuestionText : TextView
+    private lateinit var questionImg : ImageView
+    private lateinit var questionText: TextView
+    private lateinit var resultText : TextView
+
+    //Opciones de botones A,B,C Y D
+    private lateinit var optionA : Button
+    private lateinit var optionB : Button
+    private lateinit var optionC : Button
+    private lateinit var optionD : Button
+
+    //Botones de navegacion
+    private lateinit var prevBtn : Button
+    private lateinit var nextBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
 
-        hintsBtn = findViewById(R.id.hint_button)
+        hintsBtn = findViewById(R.id.hint_button) //Boton de pistas
+        numQuestionText = findViewById(R.id.numQuestion_text) //Texto de pregunta actual (1/10)
+        questionImg = findViewById(R.id.question_img) //Imagen de la categoria
+        questionText = findViewById(R.id.question_text) //Texto de la pregunta
+        resultText = findViewById(R.id.result_text) //Si la pregunta fue contestada correctamente o incorrectamente
 
+        //Opciones de botones A,B,C Y D
+        optionA = findViewById(R.id.optionA_button)
+        optionB = findViewById(R.id.optionB_button)
+        optionC = findViewById(R.id.optionC_button)
+        optionD = findViewById(R.id.optionD_button)
+
+        //Botones de navegacion
+        prevBtn = findViewById(R.id.prev_button)
+        nextBtn = findViewById(R.id.next_button)
     }
 }
